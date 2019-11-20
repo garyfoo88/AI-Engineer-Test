8aff46360c959d6ae122cd4d60cd5ac60fc6ab6d
Gary <55524871+hibiki1525@users.noreply.github.com>
2019-11-20 21:35:20 +0800

    Update face_predictor.py

--- face_predictor.py
+++ face_predictor.py
@@ -1,5 +1,9 @@
 import cv2
 import cvlib as cv
+import requests
+import json
+from urllib.request import urlopen
+from PIL import Image
 
 
 # function to convert the coordinates to columns and rows
@@ -7,8 +11,16 @@ def coordinates(n):
     return n // 100
 
 
-def face_predictor(image_path):
-    image = cv2.imread(image_path)
+def face_predictor(token):
+    # get url from 'url_image'
+    web_url = 'https://hackattic.com/challenges/basic_face_detection/problem?access_token='
+    get_problem = requests.get(web_url + token).json()
+    image_url = get_problem['image_url']
+    # save the image to PNG
+    img = Image.open(urlopen(image_url))
+    img.save('image.png', 'PNG')
+    # read the image
+    image = cv2.imread('image.png')
     faces, confidences = cv.detect_face(image)
     face_tiles = []
     # loop through the detected faces
@@ -19,3 +31,5 @@ def face_predictor(image_path):
         face_tiles.append(list(result)[1:3])
 
     return face_tiles
+    # tried converting to JSON structure using 'json.dumps(face_tiles).json()' but failed =(
+
